9ce4a43fcc8f4a26e3d9ab108f59afc4405867c0
Malak <malakhmm22@gmail.com>
2023-08-30 09:53:11 +0300

    completed-Step-6

---
+++ Step_6.js
@@ -0,0 +1,16 @@
+let numberOne;
+
+let numberTwo ;
+
+let result;
+
+
+
+numberOne = prompt("Enter the first number:");
+
+numberTwo = prompt("Enter the second number:");
+
+result = numberOne % numberTwo;
+
+
+alert("the result is: " + result);
